e7ccdf06bb3d5d12d00c6ca33ae23c52277eeb77
João Paulo <joaopaulomirandamatias@users.noreply.github.com>
2022-05-03 19:23:37 -0300

    Mapeando as minhas entidades com JPA

--- src/main/java/com/mirandas/dio/santander/banklineapi/model/Correntista.java
+++ src/main/java/com/mirandas/dio/santander/banklineapi/model/Correntista.java
@@ -1,10 +1,28 @@
 package com.mirandas.dio.santander.banklineapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+
+  @Column(length = 20)
   private String cpf;
+
+  @Column(length = 60)
   private String nome;
 
+  @Embedded
   private Conta conta;
 
   public Conta getConta() {
