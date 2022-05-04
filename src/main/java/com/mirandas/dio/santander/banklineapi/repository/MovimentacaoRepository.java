7be299e3c5ca44bdb21c5c85ee03624adb94bdb4
João Paulo <joaopaulomirandamatias@users.noreply.github.com>
2022-05-03 23:41:55 -0300

    Atualização versão Final

--- src/main/java/com/mirandas/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/mirandas/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package com.mirandas.dio.santander.banklineapi.repository;
 
+import java.util.List;
+
 import com.mirandas.dio.santander.banklineapi.model.Movimentacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
-  
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
 }
--- src/main/java/com/mirandas/dio/santander/banklineapi/service/MovimentacaoService.java
+++ src/main/java/com/mirandas/dio/santander/banklineapi/service/MovimentacaoService.java
@@ -14,32 +14,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MovimentacaoService {
-  @Autowired
-  private MovimentacaoRepository repository;
-
-  @Autowired
-  private CorrentistaRepository correntistaRepository;  
-
-  public void save(NovaMovimentacao novaMovimentacao) {
-    Movimentacao movimentacao = new Movimentacao();
-
-    Double valor = novaMovimentacao.getValor();
-    if(novaMovimentacao.getTipo() == MovimentacaoTipo.DESPESA)
-      valor = valor * -1;
-
-    movimentacao.setDataHora(LocalDateTime.now());
-    movimentacao.setDescricao(novaMovimentacao.getDescricao());
-    movimentacao.setTipo(novaMovimentacao.getTipo());
-    movimentacao.setIdConta(novaMovimentacao.getIdConta()); 
-    movimentacao.setValor(valor);
-
-    Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
-
-    if(correntista != null){
-      correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
-      correntistaRepository.save(correntista);
-    }
-
-    repository.save(movimentacao);
-  }
+	@Autowired
+	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
+	public void save(NovaMovimentacao novaMovimentacao) {
+		Movimentacao movimentacao = new Movimentacao();
+		
+		//Double valor = novaMovimentacao.getTipo()==MovimentacaoTipo.RECEITA ? novaMovimentacao.getValor() : novaMovimentacao.getValor() * -1;
+		
+		Double valor = novaMovimentacao.getValor();
+		if(novaMovimentacao.getTipo() == MovimentacaoTipo.DESPESA)
+			valor = valor * -1;
+			
+		movimentacao.setDataHora(LocalDateTime.now());
+		movimentacao.setDescricao(novaMovimentacao.getDescricao());
+		movimentacao.setIdConta(novaMovimentacao.getIdConta());
+		movimentacao.setTipo(novaMovimentacao.getTipo());
+		movimentacao.setValor(valor);
+		
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
+		
+		repository.save(movimentacao);
+		
+	}
 }
+
+
